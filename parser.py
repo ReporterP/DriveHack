eb459dcfd03bf85f2e94ff90946c9f5ffd49ab46
Alex <alex_step04@mail.ru>
2022-08-26 01:25:13 +0300

    debug

--- parser.py
+++ parser.py
@@ -1,25 +1,22 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
-import re
 from datetime import datetime
-
-
-
 import pandas as pd  # для создания/дополнения файла отчёта через DataFrames
+import re
 
 # Формирование отчёта из данных от парсеров
 def update_report(data: dict, report_path = r'./report_data.csv'):
     '''Extend existing report or create a new one'''
     if report_path == "":
         report_data = pd.DataFrame(
-            columns=['company_name', 'publicated_at'])
+            columns=['company_name', 'mentioned_times'])
     else:
         report_data = pd.read_csv(report_path)
 
     # Дополнение отчёта данными
     # Словарь в DF, а DF + DF
     # print(pd.DataFrame(data.items())) # , ignore_index=True
-    report_data = pd.concat([report_data, pd.DataFrame(list(data.items()), columns=['company_name', 'mentioned_times'] )])
+    report_data = pd.concat([report_data, pd.DataFrame([data.values()],columns=['company_name', 'mentioned_times'] )])
 
     # TODO:
     # Для оптимизации имеет смысл делать сохранение в csv, только тогда, когда получен итоговый DF
@@ -27,8 +24,15 @@ def update_report(data: dict, report_path = r'./report_data.csv'):
     report_data.to_csv('report_data.csv', index=False)
 
 
+transport_st = []
+result = []
+other_st = []
 
-''' Парсеры сайтов'''
+# load the list of transport startups
+correct_list = 'https://transport.startups-list.com/'
+
+# resourses
+url = ['https://techcrunch.com/category/startups/', 'https://techstartups.com/category/latest-technology-news/transportation/', 'https://www.eu-startups.com/?s=transport']
 
 def list_load(url):
     r = req.get(url)
@@ -37,8 +41,9 @@ def list_load(url):
         name = new.select('h1')[0].text.strip()
         transport_st.append(name)
 
+
 # parser
-def parser(url, transport_st, other_st):
+def parser(url, transport_st, result, other_st):
     list_load(correct_list)
 
     for u in url:
@@ -53,7 +58,7 @@ def parser(url, transport_st, other_st):
         #         break
         
         if url.index(u) == 1:
-            for page in range(1, 10):
+            for page in range(1, 100):
                 if page >= 2:
                     u = f'https://techstartups.com/category/latest-technology-news/transportation/page/{page}/'
                 r = req.get(u)
@@ -64,14 +69,24 @@ def parser(url, transport_st, other_st):
                     date = date.replace(',', '')
                     date = datetime.strptime('April 25 2022', '%B %d %Y')
                     date = date.strftime("%Y-%m-%d")
-                    for tr in transport_st:
-                        if tr in title:
-                            # ВЫЗОВ ФУНКЦИИ ОБНОВЛЕНИЯ ОТЧЁТА ПРЯМИКОМ ИЗ ПАРСЕРА
-                            update_report({"company_name": tr, "mentioned_at": date})
-                            break
+                    # for tr in transport_st:
+                    #     if tr in title:
+                    #         result.append({"company_name": tr, "mentioned_at": date})
+                    #         # print({"company_name": tr, "mentioned_at": date})
+                    #         break
                     if 'startup' in title:
                         if '$' in title:
-                            res = re.findall(r'(.*?) \w* [$]', title)
+                            # print(title)
+                            find_str = re.findall(r'startup(.*?)[$]', title)
+                            if len(find_str):
+                                find_str = find_str[0].replace('\xa0', ' ').strip().split(' ')
+                                # print(f'"{find_str}"')
+                                w = 0
+                                res = ''
+                                while find_str[w][0].isupper():
+                                    res += f'{find_str[w]} '
+                                    w +=1
+                            res = res.strip()        
                             update_report({"company_name": res, "mentioned_at": date})
 
         # if url.index(u) == 2:
@@ -80,14 +95,8 @@ def parser(url, transport_st, other_st):
         #         date = new.select('.entry-date')[0].text.strip()
         #         print(title, data)
         #         break
+            
 
-transport_st = [] # транспортные стартапы
-other_st = [] # В будущем для проверки является ли стартап транспортным.
-
-# load the list of transport startups
-correct_list = 'https://transport.startups-list.com/'
-
-# resourses
-url = ['https://techcrunch.com/category/startups/', 'https://techstartups.com/category/latest-technology-news/transportation/', 'https://www.eu-startups.com/?s=transport']
-
-parser(url, transport_st, other_st)
+# data_parser = parser(url, transport_st, result, other_st)
+parser(url, transport_st, result, other_st)
+# print(data_parser)
--- stats.py
+++ stats.py
@@ -1,5 +1,12 @@
+from collections import ChainMap  # для обработки данных от парсеров сайта
 import pandas as pd  # для создания/дополнения файла отчёта через DataFrames
-from datetime import date #
+
+from datetime import date
+
+import parser as par
+
+# Формирование отчёта из данных от парсеров
+
 
 
 # Выгрузка топ 10 (словарь + csv)
@@ -14,7 +21,7 @@ def get_best(start_date: str, final_date: str, report_path = r'./report_data.csv
     stats = {}
 
     for _, row in report_data.iterrows():
-        if start_date <= date.fromisoformat(row["mentioned_at"]) <= final_date:
+        if start_date <= date.fromisoformat(row["mentioned_times"]) <= final_date:
             stats[row['company_name']] = stats.get(row['company_name'], 0) + 1
 
     stats = sorted(stats.items(), key=lambda x: x[1])
@@ -33,13 +40,36 @@ def get_best(start_date: str, final_date: str, report_path = r'./report_data.csv
     return stats
 
 
+# Только для теста update_report
+'''
+data = ChainMap(
+[
+  {
+    "company_name": "compan 1",
+    "mentioned_at": "22.11.2004",
+  },
+  {
+    "company_name": "company 2",
+    "mentioned_at": "23.11.2004",
+  },
+  {
+    "company_name": "company 3",
+    "mentioned_at": "24.11.2004",
+  }
+]
+)
+
+report_path = r'G:\GitHub repos\DriveHack\report_data.csv'
+'''
+
 # Только для теста get_best
 '''
-
-report_path = r'./report_data.csv'
-
-start_date = "2000-01-01"
-final_date = "2030-01-01"
-
-print(get_best(start_date, final_date, report_path))
 '''
+
+
+# report_path = r'./report_data.csv'
+
+# start_date = "2000-01-01"
+# final_date = "2012-01-01"
+
+# print(get_best(start_date, final_date, report_path))
