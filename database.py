263c8b44a31f77b51e553ab56ff7b26314fb7691
Alex <alex_step04@mail.ru>
2022-08-25 12:07:23 +0300

    start

--- database.py
+++ database.py
@@ -6,14 +6,14 @@ config = dotenv_values(".env")
 
 class Database:
     pass
-    def __init__(self):
-        self.connection = psycopg2.connect(
-        database = os.environ.get('DATABASE', config["DATABASE"]), 
-        user = os.environ.get('USER', config["USER"]),
-        password = os.environ.get('PASSWORD', config["PASSWORD"]), 
-        host = os.environ.get('HOST', config["HOST"]), 
-        port = int(os.environ.get('PORT',config["PORT"]))
-        )
-        self.cursor = self.connection.cursor()
+    # def __init__(self):
+    #     self.connection = psycopg2.connect(
+    #     database = os.environ.get('DATABASE', config["DATABASE"]), 
+    #     user = os.environ.get('USER', config["USER"]),
+    #     password = os.environ.get('PASSWORD', config["PASSWORD"]), 
+    #     host = os.environ.get('HOST', config["HOST"]), 
+    #     port = int(os.environ.get('PORT', config["PORT"]))
+    #     )
+    #     self.cursor = self.connection.cursor()
 
     
