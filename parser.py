37488e98341a76da5fb7b037f280af3e867a0ee6
Alex <alex_step04@mail.ru>
2022-08-26 12:23:11 +0300

    parser

--- parser.py
+++ parser.py
@@ -4,21 +4,39 @@ from datetime import datetime
 import pandas as pd  # для создания/дополнения файла отчёта через DataFrames
 import re
 
-# load the list of transport startups
-# correct_list = 'https://transport.startups-list.com/'
-# def list_load(url):
-#     r = req.get(url)
-#     html = bs(r.text, 'html.parser')
-#     for new in html.select('.card'):
-#         name = new.select('h1')[0].text.strip()
-#         transport_st.append(name)
+
+transport_st = []
+
+# resourses
+url = ['https://techstartups.com/category/latest-technology-news/transportation/', 'https://www.eu-startups.com/?s=transport', 'https://inc42.com/buzz/?s=transport%20startup', 'https://www.geekwire.com/?s=transport&orderby=relevance&order=DESC&post_type=post%2Cpage%2Cdevblog%2Cgeekwire_event%2Cgeekwire_picks%2Cspecial_coverage%2Csponsor_post&category_name=transportation', 'https://startupnews.com.au/?s=transport']
+news_selector = ['.post', '.tdb_module_loop ', '.content', 'article', 'item-details']
+error_selector = ['.inner_wrapper > .error_box', '.error404', '.ais-Hits--empty', '.error404', '.error404']
+title_selector = ['h5 > a', 'h3 > a', 'h2 > a', 'h2 > a', 'h3 > a']
+date_selector = ['.post_info_date', '.entry-date', '.date', '.published', '.entry-date']
+
+
+
+# Формирование отчёта из данных от парсеров
+def update_report(data: dict, report_path = r'./report_data.csv'):
+    '''Extend existing report or create a new one'''
+    if report_path == "":
+        report_data = pd.DataFrame(
+            columns=['company_name', 'mentioned_times'])
+    else:
+        report_data = pd.read_csv(report_path)
+
+    # Дополнение отчёта данными
+    # Словарь в DF, а DF + DF
+    # print(pd.DataFrame(data.items())) # , ignore_index=True
+    report_data = pd.concat([report_data, pd.DataFrame([data.values()],columns=['company_name', 'mentioned_times'] )])
+
+    # TODO:
+    # Для оптимизации имеет смысл делать сохранение в csv, только тогда, когда получен итоговый DF
+    # Т.е. парсер прекратил свою работу.
+    report_data.to_csv('report_data.csv', index=False)
 
 # Парсинг данных в DataFrame с сохранением в csv по завершению
 def parser(url, transport_st):
-    # DF заполняемый парсером
-    parsed_data = pd.DataFrame(columns=['company_name', 'mentioned_times'])
-
-    # list_load(correct_list)
     for u in url:
         ind = url.index(u)
         page = 1
@@ -28,13 +46,19 @@ def parser(url, transport_st):
                     u = f'https://techstartups.com/category/latest-technology-news/transportation/page/{page}/'
                 elif ind == 1:
                     u = f'https://www.eu-startups.com/page/{page}/?s=transport'
+                elif ind == 2:
+                    u = f'https://inc42.com/buzz/?s=transport%20startup&page={page}'
+                elif ind == 3:
+                    u = f'https://www.geekwire.com/page/{page}/?s=transport&orderby=relevance&order=DESC&post_type=post%2Cpage%2Cdevblog%2Cgeekwire_event%2Cgeekwire_picks%2Cspecial_coverage%2Csponsor_post&category_name=transportation'
+                elif ind == 4:
+                    u = f'https://startupnews.com.au/page/{page}/?s=transport'
             r = req.get(u)
-            print()
-            print(u)
-            print()
+            # print()
+            # print(u)
+            # print()
             html = bs(r.text, 'html.parser')
             if len(html.select(error_selector[ind])) == 0:
-
+                
                 for new in html.select(news_selector[ind]):
                     title = new.select(title_selector[ind])[0].text.strip()
                     date = new.select(date_selector[ind])[0].text.strip()
@@ -44,65 +68,26 @@ def parser(url, transport_st):
 
                     for tr in transport_st:
                         if tr in title:
-                            print(f't"{title}" tr"{tr}"')
-                            print({"company_name": tr, "mentioned_at": date})
+                            # print(f't"{title}" tr"{tr}"')
+                            update_report({"company_name": tr, "mentioned_at": date})
                     if 'startup' in title:
                         if '$' in title or '€' in title:
                             # print(title)
                             find_str = re.findall(r'startup(.*?)[$€]', title)
                             if len(find_str):
-                                print(title)
+                                # print(title)
                                 find_str = find_str[0].replace('\xa0', ' ').strip().split(' ')
                                 # print(f'"{find_str}"')
                                 w = 0
                                 res = ''
                                 while find_str[w][0].isupper():
                                     res += f'{find_str[w]} '
-                                    w += 1
+                                    w +=1
                                 res = res.strip()
-                                if len(res):
-                                    # Дополнение отчёта данными
-                                    startup = {"company_name": res, "mentioned_at": date}
-                                    parsed_data = pd.concat([parsed_data, pd.DataFrame([startup.values()],
-                                                                                       columns=['company_name',
-                                                                                                'mentioned_times'])])
+                                if len(res):            
+                                    update_report({"company_name": res, "mentioned_at": date})
                                     transport_st.append(res)
-                page += 10
+                page +=1
             else:
                 break
 
-        # if url.index(u) == 0:
-        # r = req.get(u)
-        # html = bs(r.text, 'html.parser')
-        #     for new in html.select('.post-block'):
-        #         title = new.select('h2 > a')[0].text.strip()
-        #         date = new.select('time')[0].text.strip()
-        #         print(new)
-        #         break
-
-    return parsed_data
-
-
-transport_st = []
-other_st = []
-
-# resourses
-url = ['https://techstartups.com/category/latest-technology-news/transportation/',
-       'https://www.eu-startups.com/?s=transport']
-news_selector = ['.post', '.tdb_module_loop ']
-error_selector = ['.inner_wrapper > .error_box', '.error404']
-title_selector = ['h5 > a', 'h3 > a']
-date_selector = ['.post_info_date', '.entry-date']
-
-# Парсер возращает новый DF, что содержит собранные данные.
-parsed_data = parser(url, transport_st)
-
-# Загрузка имеющегося отчёта в DF, для дополнения
-report_path = r'G:\GitHub repos\DriveHack\report_data.csv'
-report_data = pd.read_csv(report_path)
-
-# Дополнение DF отчёта данными из DF с результатами парсинга
-report_data = pd.concat([report_data, parsed_data])
-
-# Сохранение результирующего DF в csv
-report_data.to_csv('report_data.csv', index=False)
